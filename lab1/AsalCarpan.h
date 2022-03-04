9f43dbe90593a6f42bd9ff19546485a411648751
Hüseyin ÖZDAĞLI <hozdagli467@hotmail.com>
2022-03-04 18:17:04 +0300

    comments added

--- lab1/AsalCarpan.h
+++ lab1/AsalCarpan.h
@@ -12,20 +12,20 @@ public:
 		carpanlar = NULL;
 		carpanSayisi = 0;
 	}
-	AsalCarpan(Sayi& x) {
+	AsalCarpan(Sayi& x) { // constructor
 		this->sayi = x;
 		carpanlar = NULL;
 		carpanSayisi = 0;
 	}
-	int getSayi() {
+	int getSayi() { //private deðiþkenlere eriþmek için getter ve setter fonksiyonlarý
 		return sayi.getDeger();
 	}
-	int getCarpanSayisi() {
+	int getCarpanSayisi() { //private deðiþkenlere eriþmek için getter ve setter fonksiyonlarý
 		return carpanSayisi;
 	}
 
-	void carpanSayisiBul() {
-		Sayi carpan(2);
+	void carpanSayisiBul() { // carpan adlý deðiþkeni 2 den baþlatýp sayýmýza bölündüðü ve asal olduðu sürece bölüyoruz böylece 
+		Sayi carpan(2);      // kaç tane çarpaný olduðunu tespit ediyoruz
 		int bolum = sayi.getDeger();
 		while (bolum != 1) {
 			int x = carpan.getDeger();
@@ -38,21 +38,21 @@ public:
 	}
 
 	void islem() {
-		carpanlar = new Sayi * [sizeof(Sayi) * (carpanSayisi)];
-		Sayi carpan(2);
+		carpanlar = new Sayi * [sizeof(Sayi) * (carpanSayisi)]; // (çarpan sayýsý x sayi nesnesinin boyutu) þeklinde asal çarpanlarý dolduracak bir
+		Sayi carpan(2);                                        // dizi oluþturuyoruz
 		int bolum = sayi.getDeger();
 		int i = 0;
 		while (bolum != 1) {
 			int x = carpan.getDeger();
 			while ((bolum % x == 0) && carpan.asalMi()) {
 				bolum = bolum / x;
-				carpanlar[i++] = new Sayi(x);
-			}
+				carpanlar[i++] = new Sayi(x); // carpanSayisiBul() fonksiyonunun aynýsý ancak burada bulduðumuz asal sayý çarpanlarýný
+			}								  // carpanlar adlý dizimize ekliyoruz
 			carpan.setDeger(x + 1);
 		}
 	}
 
-	friend ostream& operator<<(ostream& out, AsalCarpan& ac) {
+	friend ostream& operator<<(ostream& out, AsalCarpan& ac) { // sayýnýn asal sayý çarpanlarýný yazdýrmak için << operator overloading
 		out << ac.sayi << " sayisinin asal carpanlari: ";
 		for (int i = 0; i < ac.carpanSayisi - 1;i++) {
 			out << *ac.carpanlar[i] << "*";
@@ -61,7 +61,7 @@ public:
 		return out;
 	}
 
-	~AsalCarpan() {
+	~AsalCarpan() { // belleðimizi verimli bir þekilde kullanmak için carpanlar dizisini bellekten temizliyoruz
 		delete[] carpanlar;
 	}
 	
--- lab1/Sayi.h
+++ lab1/Sayi.h
@@ -6,15 +6,15 @@ class Sayi
 private:
 	int deger;
 public:
-	void setDeger(int x) {
+	void setDeger(int x) { //private deðiþkenlere eriþmek için getter ve setter fonksiyonlarý
 		deger = x;
 	}
-	int getDeger() {
+	int getDeger() { //private deðiþkenlere eriþmek için getter ve setter fonksiyonlarý
 		return deger;
 	}
 	Sayi() {
 	}
-	Sayi(int x) {
+	Sayi(int x) { // negatif sayý girilmemesi için constructurda düzenleme, negatif sayý gelirse sayýnýn deðeri otomatik olarak 1 oluyor
 		if (x < 0) {
 			deger = 1;
 		}
@@ -23,17 +23,17 @@ public:
 		}
 	}
 
-	friend ostream& operator<<(ostream& out, Sayi& x) {
+	friend ostream& operator<<(ostream& out, Sayi& x) { // sayýnýn deðerini yazdýr fonksiyonu kullanmadan yazdýrmak için << operator overloading
 		return out << x.deger;
 	}
 
-	bool asalMi() {
-		bool asalmi = true;
-		if (deger == 0 || deger == 1) {
+	bool asalMi() { 
+		bool asalmi = true; // sayýya asaldýr diyoruz
+		if (deger == 0 || deger == 1) { //0 ve 1 asal sayý olamayacaðý için asal deðildir
 			asalmi = false;
 		}
-		for (int i = 2; i < deger;i++) {
-			if (deger % i == 0) {
+		for (int i = 2; i < deger;i++) { // i'yi 2 den baþlatarak sayýnýn kendisine kadar kalansýz bölmeyi bulmayý çalýþýyoruz,
+			if (deger % i == 0) { // eðer kalansýz bölünüyorsa sayý asal deðildir diyerek fonksiyonu bitiriyoruz
 				asalmi = false;
 				break;
 			}
