fedd754100a3c710e2b1578c060d0969a05a7cb7
Huseyin Ozdagli <hozdagli467@gmail.com>
2022-03-04 16:47:11 +0300

    .

--- lab1/AsalCarpan.h
+++ lab1/AsalCarpan.h
@@ -25,12 +25,42 @@ public:
 	}
 
 	void carpanSayisiBul() {
-		int carpan = 2;
+		Sayi carpan(2);
 		int bolum = sayi.getDeger();
 		while (bolum != 1) {
-			
+			while (bolum % carpan.getDeger() == 0 && carpan.asalMi()) {
+				bolum = bolum / carpan.getDeger();
+				carpanSayisi++;
+			}
+			carpan.setDeger(carpan.getDeger() + 1);
 		}
+	}
 
+	void islem() {
+		carpanlar = new Sayi * [sizeof(Sayi) * (carpanSayisi)];
+		Sayi carpan(2);
+		int bolum = sayi.getDeger();
+		int i = 0;
+		while (bolum != 1) {
+			while (bolum % carpan.getDeger() == 0 && carpan.asalMi()) {
+				bolum = bolum / carpan.getDeger();
+				carpanlar[i++] = new Sayi(carpan.getDeger());
+			}
+			carpan.setDeger(carpan.getDeger() + 1);
+		}
+	}
+
+	friend ostream& operator<<(ostream& out, AsalCarpan& ac) {
+		out << ac.sayi << " sayisinin asal carpanlari: ";
+		for (int i = 0; i < ac.carpanSayisi - 1;i++) {
+			out << *ac.carpanlar[i] << "*";
+		}
+		out << *ac.carpanlar[ac.carpanSayisi - 1] << endl;
+		return out;
+	}
+
+	~AsalCarpan() {
+		delete[] carpanlar;
 	}
 	
 };
--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -8,7 +8,7 @@ int main()
     cout << s << " sayisi asal mi: " << s.asalMi() << endl;
 
     AsalCarpan* as1 = new AsalCarpan(s);
-    as1->carpanSayisi();
+    as1->carpanSayisiBul();
     as1->islem();
     cout << *as1;
     as1->~AsalCarpan();
